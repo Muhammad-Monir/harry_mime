b71195007c3cfa7cb926ffc07581ceca2b1df70b
Md Ridoy <60496912+ridoy227@users.noreply.github.com>
2024-07-08 12:05:05 +0600

    ui update

--- lib/features/onboarding/presentation/onboarding.dart
+++ lib/features/onboarding/presentation/onboarding.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:harry_mine/common_widgets/auth_button.dart';
 import 'package:harry_mine/constants/text_font_style.dart';
 import 'package:harry_mine/gen/assets.gen.dart';
 import 'package:harry_mine/helpers/ui_helpers.dart';
@@ -31,10 +32,12 @@ class Onboarding extends StatelessWidget {
                style: TextFontStyle.headline32FFFFFFRoboto,
                ),
 
-               Text("Business Ideas Generator",
+               Text("Discover innovative concepts, explore new industries, and embark on your journey to success.",
                textAlign: TextAlign.center,
-               style: TextFontStyle.headline32FFFFFFRoboto,
+               style: TextFontStyle.headline16cDFE1E6Poppins,
                ),
+
+               AuthCustomeButton(name: "GET STARTED", onCallBack: (){}, height: 20.h, minWidth: 100.w, borderRadius: 10, color: Colors.blue, textStyle: TextFontStyle.headline32FFFFFFRoboto, context: context)
 
 
 
